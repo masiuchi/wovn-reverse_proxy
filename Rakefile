713ceca74735196bba405c4ea4906515a5c1bd4a
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-08-27 23:48:36 +0900

    Add tests.

--- Rakefile
+++ Rakefile
@@ -1,2 +1,12 @@
-require "bundler/gem_tasks"
-task :default => :spec
+# coding: UTF-8
+
+require 'bundler/gem_tasks'
+require 'rake/testtask'
+
+task 'default' => :test
+
+Rake::TestTask.new do |test|
+  test.libs << 'test'
+  test.test_files = Dir['test/**/spec_*.rb']
+  test.verbose = true
+end
---
+++ test/spec_wovnrb.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require 'rack/lint'
+require 'rack/mock'
+require 'wovn/reverse_proxy/wovnrb'
+
+describe Wovn::ReverseProxy::Wovnrb do
+  it 'inherits Wovnrb::Interceptor' do
+    Wovn::ReverseProxy::Wovnrb.superclass.must_equal Wovnrb::Interceptor
+  end
+
+  it 'has setting_keys' do
+    middleware = Wovn::ReverseProxy::Wovnrb.new(app)
+    setting_keys = middleware.instance_variable_get('@setting_keys')
+    setting_keys.must_include 'user_token'
+    setting_keys.must_include 'secret_key'
+    setting_keys.must_include 'url_pattern'
+  end
+
+  it 'resets its settings in every request' do
+    middleware = Wovn::ReverseProxy::Wovnrb.new(app)
+    middleware.call env('HTTP_X_WOVN_URL_PATTERN' => 'subdomain')
+    settings(middleware)['url_pattern'].must_equal 'subdomain'
+    middleware.call env
+    settings(middleware)['url_pattern'].must_equal 'path'
+  end
+
+  it 'has "use_proxy = true" setting' do
+    middleware = Wovn::ReverseProxy::Wovnrb.new(app)
+    middleware.call env('HTTP_X_WOVN_USE_PROXY' => 'false')
+    settings(middleware)['use_proxy'].must_equal true
+  end
+
+  def settings(middleware)
+    middleware.instance_variable_get('@store').settings
+  end
+
+  def env(opts = {})
+    Rack::MockRequest.env_for('/', opts)
+  end
+
+  def app
+    proc { [200, { 'Content-Type' => 'text/plain' }, ['Hello, World!']] }
+  end
+end
